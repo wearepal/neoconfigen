22f45fc567dfe62bd94939f471a851aa9fed7a95
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-02-23 11:42:43 +0000

    Update setup.py.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="hydra-configen",
-    version="0.9.0dev5",
+    name="configen",
+    version="1.0.0",
     packages=find_packages(include=["configen"]),
     entry_points={"console_scripts": ["configen = configen.configen:main"]},
-    author="Omry Yadan",
-    author_email="omry@fb.com",
-    url="http://hydra.cc",
+    author="Omry Yadan, Rosario Scalise",
+    author_email="omry@fb.com, rosario@cs.uw.edu",
+    url="https://github.com/predictive-analytics-lab/neoconfigen",
     include_package_data=True,
-    install_requires=["hydra-core>=1.0.0", "jinja2"],
+    install_requires=["hydra-core>=1.1.0dev1", "jinja2"]
 )
