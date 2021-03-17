df7260313fb234e8c82c0170fcdc9bc401956e95
Thomas M <thomas5max@gmail.com>
2021-03-17 11:48:52 +0000

    Try to make CI pass

--- configen/utils.py
+++ configen/utils.py
@@ -1,6 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import sys
-from collections import defaultdict
 from enum import Enum
 from typing import Any, List, Optional, Set, Tuple, Type, get_args, get_origin
 
